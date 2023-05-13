657cea6e6213a5def8d7d108633bc1e427b164da
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2023-05-13 08:56:57 +0530

    updated base profile for nilext operations

--- base-profile.sh
+++ base-profile.sh
@@ -5,7 +5,12 @@ FirstServerPort=17070 # change it when only necessary (i.e., firewall blocking, 
 NumOfServerInstances=5 # before recompiling, try no more than 5 servers. See Known Issue # 4
 NumOfClientInstances=2 #20, 10, 5, 2
 reqsNb=20000
-writes=50
+
+# For Nilext operations:
+# 1. writes = 0 for 100% reads
+# 2. writes = 100 for 100% writes
+writes=50 #non nilext
+
 dlog=false
 conflicts=0
 thrifty=false
